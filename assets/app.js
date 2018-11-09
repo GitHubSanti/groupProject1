43de0a37e5f0f04598384d59de71a97baee27e54
tjohn130 <tjohn130@email.cpcc.edu>
2018-11-08 21:31:30 -0500

    testing

--- assets/app.js
+++ assets/app.js
@@ -7,11 +7,23 @@ $(document).ready(function(){
     queryparam.entity_id = "301";
     var queryurl = $.param(queryparam)
     
-    var http = require('http');
-    http.createServer(function (queryurl, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(queryurl.url);
-    res.end();
-    }).listen(8080);
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?&location=charlotte";
+
+
+
+       $.ajax({
+          url: myurl,
+          headers: {
+           'Authorization':'Bearer hMCpjFUoT7ODiFsjTHANudhiTR_64AdWbQLNNwwZjuEcEiaHIROcy2OXbpXEKsuCRd3KDmiflByIdZWJQY4klSRMwR7nkcuD82do3vhdTl5DZME2meZ37I16VtvkW3Yx',
+       },
+          method: 'GET',
+          dataType: 'json',
+          success: function(data){
+           for (var i = 0; i < data.businesses.length; i++) {
+
+           console.log(data.businesses[i].name);
+           }
+          }
+       });
 
     })
--- assets/node.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-    http.createServer(function (queryurl, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(queryurl.url);
-    res.end();
-    }).listen(8080);
