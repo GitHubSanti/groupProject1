f64d2233fa3c62a2c336bd5c29c36898adac096d
tjohn130 <tjohn130@email.cpcc.edu>
2018-11-10 11:37:28 -0500

    ajax call 1.02

--- assets/app.js
+++ assets/app.js
@@ -4,22 +4,28 @@ $(document).ready(function(){
 $("#submit-btn").click(function(e){
     e.preventDefault()
     var location = $("#search-input").val().trim()
-    console.log(location);
-
     var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location="+location;
 
-    console.log(myurl)
-
 $.ajax({
-url: myurl,
-headers: {
-    'Authorization':'Bearer hMCpjFUoT7ODiFsjTHANudhiTR_64AdWbQLNNwwZjuEcEiaHIROcy2OXbpXEKsuCRd3KDmiflByIdZWJQY4klSRMwR7nkcuD82do3vhdTl5DZME2meZ37I16VtvkW3Yx',
-},
-method: 'GET',
-dataType: 'json',
-success: function(data){
-    console.log(data);
-}
+    url: myurl,
+    headers: {
+        'Authorization':'Bearer hMCpjFUoT7ODiFsjTHANudhiTR_64AdWbQLNNwwZjuEcEiaHIROcy2OXbpXEKsuCRd3KDmiflByIdZWJQY4klSRMwR7nkcuD82do3vhdTl5DZME2meZ37I16VtvkW3Yx',
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+        console.log(data.businesses);
+        data.businesses.forEach(element => {
+            console.log(element.display_phone)
+            console.log(element.name)
+            console.log(element.image_url)
+            console.log(element.rating)
+            console.log(element.location.display_address)
+
+        });
+        
+        
+        }
     })
 })
 
