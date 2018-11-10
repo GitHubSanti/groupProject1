c54df67abf60b87e2963013975b16d775de760c0
tjohn130 <tjohn130@email.cpcc.edu>
2018-11-10 10:38:13 -0500

    testing

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,19 @@
-$(document).ready(function(){
-
-    
-    var typeSearch = "/search";
-    var queryparam = "https://developers.zomato.com/api/v2.1"+typeSearch;
-    queryparam.q = "food";
-    queryparam.entity_id = "301";
-    var queryurl = $.param(queryparam)
-    
-    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?&location=charlotte";
+$(document).ready(function(){ 
+var location = "charlotte"
 
 
-
-       $.ajax({
-          url: myurl,
-          headers: {
-           'Authorization':'Bearer hMCpjFUoT7ODiFsjTHANudhiTR_64AdWbQLNNwwZjuEcEiaHIROcy2OXbpXEKsuCRd3KDmiflByIdZWJQY4klSRMwR7nkcuD82do3vhdTl5DZME2meZ37I16VtvkW3Yx',
-       },
-          method: 'GET',
-          dataType: 'json',
-          success: function(data){
-           for (var i = 0; i < data.businesses.length; i++) {
-
-           console.log(data.businesses[i].name);
-           }
-          }
-       });
-
+var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location="+location;
+$.ajax({
+   url: myurl,
+   headers: {
+    'Authorization':'Bearer hMCpjFUoT7ODiFsjTHANudhiTR_64AdWbQLNNwwZjuEcEiaHIROcy2OXbpXEKsuCRd3KDmiflByIdZWJQY4klSRMwR7nkcuD82do3vhdTl5DZME2meZ37I16VtvkW3Yx',
+},
+   method: 'GET',
+   dataType: 'json',
+   success: function(data){
+       console.log(data);
+   }
     })
+
+    
+})
