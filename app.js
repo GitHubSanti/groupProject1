66d65d0f65b929545a596cc77989048c05bd5269
Spiderman <santiagosepulveda@me.com>
2018-11-10 12:02:08 -0500

    Revert "Resturant list results"
    This reverts commit b773f379f51225f5f9c019a70030d95e2f37ef80.

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-var baseUrl = 'https://developers.zomato.com/api/v2.1/cities'
-var data = {
-    q: 'boston',
-};
-
-var headers = {
-    user_key: '751388b7b15b5749d770a8b4eecd2816'
-}
-
-$.ajax({
-    url: baseUrl,
-    data: data,
-    headers: headers,
-}).done(function(response) {
-    console.log(response);
-})
+$(document).ready(function() {});
