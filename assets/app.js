fc2b256115164b1b3688c65481f90354f68dd8a6
tjohn130 <tjohn130@email.cpcc.edu>
2018-11-10 12:40:09 -0500

    new wireframe

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ $("#submit-btn").click(function(e){
     e.preventDefault()
     var location = $("#search-input").val().trim()
     var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location="+location;
-
+    
 $.ajax({
     url: myurl,
     headers: {
@@ -15,13 +15,20 @@ $.ajax({
     dataType: 'json',
     success: function(data){
         console.log(data.businesses);
+        $("#resturant-results").empty()
         data.businesses.forEach(element => {
-            console.log(element.display_phone)
-            console.log(element.name)
-            console.log(element.image_url)
-            console.log(element.rating)
-            console.log(element.location.display_address)
+            var imgNew = $("<img>")
+            var divItem = $("<div>")
+            var pNew = $("<p>")
 
+            pNew.prepend(element.name)
+            pNew.prepend(element.display_phone)
+            pNew.prepend(element.image_url)
+            pNew.prepend(element.rating)
+            console.log(element.location.display_address)
+            imgNew.attr("src", element.image_url);
+            divItem.append(imgNew)
+            $("#resturant-results").append(divItem);
         });
         
         
