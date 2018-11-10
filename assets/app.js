0d99dcc2d6114a4b097ef78e0b0ac3990e4f2933
tjohn130 <tjohn130@email.cpcc.edu>
2018-11-10 11:02:27 -0500

    intial ajax call

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){ 
-var location = "charlotte"
 
 
-var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location="+location;
+$("#submit-btn").click(function(e){
+    e.preventDefault()
+    var location = $("#search-input").val().trim()
+    console.log(location);
+
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location="+location;
+
+    console.log(myurl)
+
 $.ajax({
-   url: myurl,
-   headers: {
+url: myurl,
+headers: {
     'Authorization':'Bearer hMCpjFUoT7ODiFsjTHANudhiTR_64AdWbQLNNwwZjuEcEiaHIROcy2OXbpXEKsuCRd3KDmiflByIdZWJQY4klSRMwR7nkcuD82do3vhdTl5DZME2meZ37I16VtvkW3Yx',
 },
-   method: 'GET',
-   dataType: 'json',
-   success: function(data){
-       console.log(data);
-   }
+method: 'GET',
+dataType: 'json',
+success: function(data){
+    console.log(data);
+}
     })
+})
+
+
+
 
     
 })
