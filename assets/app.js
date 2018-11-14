60ba111032feca5e7fac8368086aa8f144ff5c28
tjohn130 <tjohn130@email.cpcc.edu>
2018-11-13 20:30:40 -0500

    update

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@ $("#list").hide()
 $("#submit-btn").click(function(e){
     e.preventDefault()
     var location = $("#search-input").val().trim()
-    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location="+location;
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&by-chloe&location="+location;
     
 $.ajax({
     url: myurl,
@@ -19,22 +19,46 @@ $.ajax({
         console.log(data.businesses);
         $("#resturant-results").empty()
         data.businesses.forEach(element => {
-            var imgNew = $("<img>")
-            var divItem = $("<div>")
-            var pNew = $("<p>")
-            
-            pNew.prepend(element.name)
-            divItem.append(pNew)
-            pNew.prepend(element.display_phone)
-            divItem.append(pNew)
-            pNew.prepend(element.rating)
-            console.log(element.location.display_address)
-            imgNew.attr("src", element.image_url)
-            divItem.append(imgNew)
-            $("#resturant-results").append(divItem);
+            // Create necessary layout show individual resturant data
+          var divDivider = $("<div class='divider'>");
+          var divSection = $("<div class='section'>");
+          var divRow = $("<div class='row'>");
+          var divSectionLeftCol = $("<div class='col s8'>");
+          var divSectionRightCol = $("<div class='col s4 mt-4'>");
+          // Column 1 items
+          var resturantName = $("<h5 id='resturantName' class=''>");
+          var resturantRating = $("<p>");
+          var resturantPrice = $("<p>");
+          resturantName.text(element.name);
+          resturantRating.text(
+            element.rating + "/5 " + element.review_count + " reviews"
+          );
+          resturantPrice.text(element.price);
+          // Column 2 items
+          var resturantNumber = $("<p>");
+          resturantNumber.text(element.display_phone);
+
+          $("#resturant-results").append(divDivider);
+          $("#resturant-results").append(divSection);
+          // Append to column1 in resturant section (resturant name, resturant rating, resturant price)
+          divSection.append(divRow);
+          divRow.append(divSectionLeftCol);
+          divRow.append(divSectionRightCol);
+          divSectionLeftCol.append(resturantName);
+          divSectionLeftCol.append(resturantRating);
+          divSectionLeftCol.append(resturantPrice);
+          // Append to column2 in resturant section
+          divSectionRightCol.append(resturantNumber);
+
+          element.location.display_address.forEach(displayAddressLine => {
+            var resturantAddressline = $("<p>");
+            resturantAddressline.text(displayAddressLine);
+            divSectionRightCol.append(resturantAddressline);
+          });
+
         });
 
-        $("#search").hide(2000)
+        $("#websearch").hide(2000)
         $("#list").show(3000)
         
         }
